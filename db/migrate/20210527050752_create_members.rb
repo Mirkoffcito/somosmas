c05ba4b7f604ff30365cd4defb7f60eb02c51be4
juan Illingworth <jpillingworth@hotmail.com>
2021-05-27 14:06:51 -0300

    Changed CamelCase to snake_case following conventions

--- db/migrate/20210527050752_create_members.rb
+++ db/migrate/20210527050752_create_members.rb
@@ -2,9 +2,9 @@ class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
       t.string :name, null: false
-      t.string :facebookUrl
-      t.string :instagramUrl
-      t.string :linkedinUrl
+      t.string :facebook_url
+      t.string :instagram_url
+      t.string :linkedin_url
       t.string :description
       t.datetime :deleted_at
 
