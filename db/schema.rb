75e2acc58536d285eacb0a430463a731a7ed2aa8
Lucia diez <lupe.diez@gmail.com>
2021-05-27 10:56:59 -0300

    creating migration to fix problems in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_111906) do
+ActiveRecord::Schema.define(version: 2021_05_27_135345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,15 +49,6 @@ ActiveRecord::Schema.define(version: 2021_05_27_111906) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_categories_on_deleted_at"
-  end
-
-  create_table "news", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_news_on_deleted_at"
   end
 
   create_table "organizations", force: :cascade do |t|
