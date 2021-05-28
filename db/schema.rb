722e85c91b8f8f836a5d7545db3bebf0ba267665
Jose Figueroa <joselfigueroag@gmail.com>
2021-05-28 12:08:55 -0400

    schema fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_131946) do
+ActiveRecord::Schema.define(version: 2021_05_28_135112) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,6 @@ ActiveRecord::Schema.define(version: 2021_05_28_131946) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_categories_on_deleted_at"
   end
 
   create_table "members", force: :cascade do |t|
@@ -94,7 +92,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_131946) do
     t.string "last_name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
-    t.bigint "role_id", null: false
+    t.bigint "role_id"
     t.datetime "deleted_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
