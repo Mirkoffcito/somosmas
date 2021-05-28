e92b37e932e393ffa229e6de4aba506a609580a0
Jose Figueroa <joselfigueroag@gmail.com>
2021-05-28 18:32:04 -0400

    correction in compared parameter and inclusion of authorization method in application controller

--- app/controllers/api/authentications_controller.rb
+++ app/controllers/api/authentications_controller.rb
@@ -1,14 +1,14 @@
-class AuthenticationsController < ApplicationController
+class Api::AuthenticationsController < ApplicationController
   before_action :authorize_request, except: :login
 
   def login
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token: token }, status: :ok
     else
-      render json: { ok: false }
+      render json: { error: "Invalid user or password" }
     end
   end
 
