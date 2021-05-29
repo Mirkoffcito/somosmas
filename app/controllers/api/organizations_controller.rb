0493c1c43c9af7f25d3ca83882a7b6f7c572ccb4
Jose Figueroa <joselfigueroag@gmail.com>
2021-05-28 22:29:38 -0400

    example with endpoint organization

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -1,8 +1,14 @@
 class Api::OrganizationsController < ApplicationController
+  before_action :authorize_request
+  before_action :user_authorize
 
   def index
     @organization = Organization.first
-
-    render json: @organization, serializer: OrganizationSerializer
+    
+    if user_authorize
+      render json: @organization, serializer: OrganizationSerializer
+    else
+      render json: { error: "No eres Administrador" }, status: :unauthorized
+    end
   end
 end
