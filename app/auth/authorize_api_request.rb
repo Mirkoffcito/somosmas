d93ec8aa0c4a0caaff491a56f719101769c5e0a1
Jose Figueroa <joselfigueroag@gmail.com>
2021-05-28 22:28:00 -0400

    method for determine who is admin, change from class to module. Include in application controller

--- app/auth/authorize_api_request.rb
+++ app/auth/authorize_api_request.rb
@@ -1,16 +1,17 @@
-class AuthorizeApiRequest
+module AuthorizeApiRequest
 
   def authorize_request
     header = request.headers['Authorization']
     header = header.split(' ').last if header
-    begin
-      @decoded = JsonWebToken.decode(header)
-      @current_user = User.find(@decoded[:user_id])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
-      render json: { message: 'Unauthorized access.' }, status: :unauthorized
-    end
+    @decoded = JsonWebToken.decode(header)
+    @current_user = User.find(@decoded[:user_id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { errors: e.message }, status: :unauthorized
+  rescue JWT::DecodeError => e
+    render json: { message: 'Unauthorized access.' }, status: :unauthorized
   end
 
+  def user_authorize
+    @current_user.isadmin?
+  end
 end
