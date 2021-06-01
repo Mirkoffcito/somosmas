b4f5a56a47343779e690e8df7b085c7351fad1a8
leonsantiago <santiagoleon747@gmail.com>
2021-06-01 19:26:27 -0300

    remove puts on sent method

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -12,8 +12,6 @@ class Mailer < ApplicationMailer
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.headers
   end
 
 end
