8a72712ad1de916c3397e10897637f2a32f2fecc
leonsantiago <santiagoleon747@gmail.com>
2021-06-02 12:39:31 -0300

    Added closures to params

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,16 +2,14 @@ class Mailer < ApplicationMailer
 
   require 'sendgrid-ruby'
   include SendGrid
-
-
-  def sent email, name, subject, content
+  def sent (email, name, subject, content)
     from = SendGrid::Email.new(email: ENV['SENDER_MAIL'], name: "Alkemy")
     to = SendGrid::Email.new(email: email, name: name)
     content = SendGrid::Content.new(type: 'text/html', value: content)
     mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    sg.client.mail._('send').post(request_body: mail.to_json)
   end
 
 end
