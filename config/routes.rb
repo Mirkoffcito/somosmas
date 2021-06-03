3a4aea5170022e3bb42170be11a67df6326b6773
Guido Medina <guido10md@gmail.com>
2021-06-02 21:10:25 -0300

    pulled from development

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,8 @@ Rails.application.routes.draw do
     post 'auth/login', to: 'authentications#login'
     get 'auth/me', to: 'users#show'
     get 'users', to: 'users#index'
-<<<<<<< HEAD
     delete 'users/:id', to: 'users#destroy'
-=======
     patch 'organization/public', to: 'organizations#update'
->>>>>>> 4944de0 (pulled from development)
   end
   
 end
