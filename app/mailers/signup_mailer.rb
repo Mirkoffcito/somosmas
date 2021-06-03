851a4e9ed75ab5129a217d4738c447496ede2cd9
Cristian Oostdijk <cfo.oostdijk@gmail.com>
2021-06-03 15:01:38 +0000

    signup_mailer.rb edited online with Bitbucket

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -9,10 +9,10 @@ class SignupMailer< ApplicationMailer
     mail.from = Email.new(email: ENV['SENDER_MAIL'])
     mail.template_id = ENV['SENDGRID_SIGNUP_TEMPLATE_ID']
     personalization = Personalization.new
-    personalization.add_to(Email.new(email: '@user.email'))
+    personalization.add_to(Email.new(email: @user.email))
     personalization.add_dynamic_template_data({
     "subject" => "Bienvenido a Somos Mas",
-    "first_name" => "@user.first_name"})
+    "first_name" => @user.first_name})
     mail.add_personalization(personalization)
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY']) 
