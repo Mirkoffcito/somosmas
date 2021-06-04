e2aa008ff89fd56feac18dda76a0d455ef8897b9
Santiago Leon <santiagoleon747@gmail.com>
2021-06-03 23:32:24 -0300

    fix for slide model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_191932) do
+ActiveRecord::Schema.define(version: 2021_06_04_022335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,15 @@ ActiveRecord::Schema.define(version: 2021_06_02_191932) do
   create_table "roles", force: :cascade do |t|
     t.string "name", null: false
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "slides", force: :cascade do |t|
+    t.string "image_url", null: false
+    t.string "text"
+    t.bigint "order"
+    t.bigint "organization_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
