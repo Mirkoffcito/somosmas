6635a2936b1a41534c8573d37774eb9ccaf7e603
Jose Figueroa <joselfigueroag@gmail.com>
2021-06-08 11:31:22 -0400

    included methodo for notFound error

--- app/controllers/concerns/errors_helper.rb
+++ app/controllers/concerns/errors_helper.rb
@@ -1,9 +1,14 @@
 module ErrorsHelper extend ActiveSupport::Concern
   included do
     rescue_from ActionController::ParameterMissing, with: :parameter_missing
-    
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
     def parameter_missing
       render json: {error: 'Parameter is missing or its value is empty'}, status: :bad_request
+    end
+
+    def not_found
+      render json: {error:'Resource not found'}, status: :not_found
     end
   end
 end
