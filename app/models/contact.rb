e905d62601b038ebdff411ca0a76331904825131
Juan Illingworth <juanpabloillingworth@gmail.com>
2021-06-09 20:26:08 +0000

    From multi-line to one-line validation for Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,4 @@
 class Contact < ApplicationRecord
   acts_as_paranoid
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :message, presence: true
+  validates :name, :email, :message, presence: true
 end
