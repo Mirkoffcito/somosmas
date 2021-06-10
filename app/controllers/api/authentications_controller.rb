321390d5f8c1ce21740374a912b424861f2b40a9
Jose Figueroa <joselfigueroag@gmail.com>
2021-06-10 14:46:42 -0400

    rubocop fixes authentications controller

--- app/controllers/api/authentications_controller.rb
+++ app/controllers/api/authentications_controller.rb
@@ -2,6 +2,15 @@
 
 module Api
   class AuthenticationsController < ApplicationController
+    def register
+      @user = User.create(user_params)
+      if @user.save
+        login
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
     def login
       @user = User.find_by_email(params[:user][:email])
       if @user&.authenticate(params[:user][:password])
@@ -10,15 +19,6 @@ module Api
         render json: @user, serializer: UserRegistrationSerializer, status: :ok
       else
         render json: { error: 'Invalid user or password' }, status: :bad_request
-      end
-    end
-
-    def register
-      @user = User.create(user_params)
-      if @user.save
-        login
-      else
-        render json: @user.errors, status: :unprocessable_entity
       end
     end
 
