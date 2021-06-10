77559c3fdf092e055faa9d820131b5a9210de32c
Jose Figueroa <joselfigueroag@gmail.com>
2021-06-10 16:38:34 -0400

    add foreign key slides table to organization table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_033407) do
+ActiveRecord::Schema.define(version: 2021_06_10_203315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,5 +133,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_033407) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "news", "categories"
+  add_foreign_key "slides", "organizations"
   add_foreign_key "users", "roles"
 end
