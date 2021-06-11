d9fdced7785f6dc95f018e164b3d0ae505e70387
Jose Figueroa <joselfigueroag@gmail.com>
2021-06-11 14:41:32 -0400

    fixes in models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Activity < ApplicationRecord
-  validates :name, presence: true
-  validates :content, presence: true
+  validates :name, :content, presence: true
   has_one_attached :image
 end
