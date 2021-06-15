2a331457753fbf72f0fd0e761ea245bb6f883480
Alkemy <alkemydevonrails@gmail.com>
2021-06-15 09:45:29 -0300

    Destroy Testimonial controller added

--- app/controllers/api/testimonials_controller.rb
+++ app/controllers/api/testimonials_controller.rb
@@ -2,5 +2,17 @@
 
 module Api
   class TestimonialsController < ApplicationController
+    before_action :authenticate_admin
+    before_action :testimonial, only: %i[destroy]
+
+    def destroy
+      render json: { message: 'Succesfully deleted' }, status: :ok if testimonial.destroy
+    end
+
+    private
+
+    def testimonial
+      @testimonial ||= Testimonial.find(params[:id])
+    end
   end
 end
