abe98e1d839ebb42bb438c227f1c6455b83c7a7c
Santiago Leon <santiagoleon747@gmail.com>
2021-06-15 10:02:50 -0300

    Endpoint for updating testimonials

--- app/controllers/api/testimonials_controller.rb
+++ app/controllers/api/testimonials_controller.rb
@@ -2,5 +2,22 @@
 
 module Api
   class TestimonialsController < ApplicationController
+    before_action :authenticate_admin, only: [:update]
+
+    def update
+      if testimonial.update(testimonial_params)
+        render json: testimonial, status: :ok
+      else
+        render json: testimonial.errors, status: :unprocessable_entity
+      end
+    end
+
+    def testimonial
+      @testimonial ||= Testimonial.find(params[:id])
+    end
+
+    def testimonial_params
+      params.require(:testimonial).permit(:name, :content)
+    end
   end
 end
