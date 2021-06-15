a22a6a657748f476cbed544b72e91deedfe3b535
Alkemy <alkemydevonrails@gmail.com>
2021-06-15 16:22:44 -0300

    Indexation fixes

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Api
-    class CommentsController < ApplicationController
-      before_action :authenticate_admin
+  class CommentsController < ApplicationController
+    before_action :authenticate_admin
 
-      def index
-        @comments = Comment.all
-        render json: @comments
-      end
-
+    def index
+      @comments = Comment.all
+      render json: @comments
     end
+
+  end
 end
