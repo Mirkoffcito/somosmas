5ec4eb2529dff310242e765211eceb0f09700d81
Alkemy <alkemydevonrails@gmail.com>
2021-06-15 11:05:46 -0300

    callback eliminated

--- app/controllers/api/testimonials_controller.rb
+++ app/controllers/api/testimonials_controller.rb
@@ -3,7 +3,6 @@
 module Api
   class TestimonialsController < ApplicationController
     before_action :authenticate_admin
-    before_action :testimonial, only: %i[destroy]
 
     def destroy
       render json: { message: 'Succesfully deleted' }, status: :ok if testimonial.destroy
