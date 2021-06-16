80be7feca5f159a0f7816627eb50f1f877fe53a9
Alkemy <alkemydevonrails@gmail.com>
2021-06-16 18:15:36 -0300

    Routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :testimonials, only: [:create, :update, :destroy]
     resources :members, only: [:index, :destroy, :create, :update]
     resources :comments, only: [:index]
+    resources :contacts, only: [:create]
   end
 
 end
