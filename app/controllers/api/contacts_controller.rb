d9d8d06b5f98cf36b8669cd6d92b86af6d7126f5
lucia <lupe.diez@gmail.com>
2021-06-16 19:24:13 -0300

    fixing errors

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,14 +1,14 @@
 module Api
-class ContactsController < ApplicationController
-    before_action ::authorize_request
+    class ContactsController < ApplicationController
+        before_action :authorize_request
 
-    def show
-            @contacts = Contact.all.select{ |contact| contact.user_id == @current_user.id }
-        if @contacts != nil?
-            render json: @contacts.all
-        else 
-            render json: @contacts.errors, status: :unprocessable_entity
-
+        def index
+                @contacts = Contact.select { |contact| contact.user_id == @current_user.id }
+            if @contacts != nil?
+                render json: @contacts
+            else 
+                render json: @contacts.errors, status: :unprocessable_entity
+            end
+        end
     end
-
 end
