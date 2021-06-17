071ec7da3765a6d78a744a614f86d5daa8ef107d
lucia <lupe.diez@gmail.com>
2021-06-17 13:51:25 -0300

    email in model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,13 @@
 class Contact < ApplicationRecord
   acts_as_paranoid
   validates :name, :email, :message, presence: true
+
+
+  after_create :send_mail
+
+  def send_mail
+    subject = 'Gracias por su contacto'
+    template = ENV['sengrid template']
+    Mailer.send_mail(self, subject, template).deliver
+  end 
 end
