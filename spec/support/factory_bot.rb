e2af4ffb432408e23ab09968284662b2a66445e5
Guido Medina <guido10md@gmail.com>
2021-06-21 18:38:29 +0000

    factory_bot.rb edited online with Bitbucket

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,5 +1,6 @@
 # With this configuration, now instead of doing
 # 'FactoryBot.build :post' we can do 'build :post'
+require 'factory_bot'
 RSpec.configure do |config|
     config.include FactoryBot::Syntax::Methods
 end
