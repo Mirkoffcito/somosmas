1808dc7aaed9c91f2d250ad75ec728fa4814d11a
Guido Medina <guido10md@gmail.com>
2021-06-21 21:22:54 -0300

    finished setup

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,6 +1,5 @@
 # With this configuration, now instead of doing
 # 'FactoryBot.build :post' we can do 'build :post'
-require 'factory_bot_rails'
 RSpec.configure do |config|
     config.include FactoryBot::Syntax::Methods
 end
