0ea8e3393fffbebc1c7385468f8cf31aef20600d
Guido Medina <guido10md@gmail.com>
2021-06-22 10:57:08 -0300

    added factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,2 +1,18 @@
 FactoryBot.define do 
+    
+    factory :user do
+        email {Faker::Internet.email}
+        first_name {Faker::Name.first_name}
+        last_name {Faker::Name.last_name}
+        password {"12345678"}
+        password_confirmation {"12345678"}
+    end
+
+    factory :invalid_user, class:'User' do
+        email {nil}
+        first_name {Faker::Name.first_name}
+        last_name {Faker::Name.last_name}
+        password {"12345678"}
+        password_confirmation {"12345678"}
+    end
 end
