40f57ae4892c1a62ade712124d51f88d83d93da0
Alkemy <alkemydevonrails@gmail.com>
2021-06-23 11:23:24 -0300

    Organization rubocop fixed

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :organization do
-    name {Faker::Company.name}
-    email {Faker::Internet.email}
-    welcome_text {Faker::Company.catch_phrase}
-    address {Faker::Address.street_name}
-    phone {Faker::PhoneNumber.phone_number}
+    name { Faker::Company.name }
+    email { Faker::Internet.email }
+    welcome_text { Faker::Company.catch_phrase }
+    address { Faker::Address.street_name }
+    phone { Faker::PhoneNumber.phone_number }
   end
 end
-    
