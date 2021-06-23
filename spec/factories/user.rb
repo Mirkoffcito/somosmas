ee482bd585da0295f240e7d14346939acc94302c
Alkemy <alkemydevonrails@gmail.com>
2021-06-23 15:21:47 -0300

    user updated

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,14 +2,6 @@
 
 FactoryBot.define do
 
-  factory :public_user, class: 'User' do
-    email { Faker::Internet.email }
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    password { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
-  end
-  
   factory :admin_user, class: 'User' do
     email { Faker::Internet.email }
     first_name { Faker::Name.first_name }
