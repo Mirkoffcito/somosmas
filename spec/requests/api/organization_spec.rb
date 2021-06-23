c8964d0ba7bc8c6a7ca7135e2628f1b11611e50e
Alkemy <alkemydevonrails@gmail.com>
2021-06-22 21:24:10 -0300

    Role, User added

--- spec/requests/api/organization_spec.rb
+++ spec/requests/api/organization_spec.rb
@@ -2,18 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "Test of Organizations Controller", type: :request do
 
-  # initialize test data
-  let!(:organization) {create(:organization)}
-
-  # Test suite for GET #index
   describe "GET /api/organization/public" do
-    before do
-      get ('/api/organization/public')
-    end
+    let!(:organization) {create(:organization)}
 
     it "as public user, returns status ok" do
+      get ('/api/organization/public')
+      expect(response).to have_http_status(:success)
+    end
+
+  end
+
+  describe "PATCH /api/organization/public" do
+    let!(:organization) {create(:organization)}
+    let!(:role) {create(:role)}
+    let!(:user) {create(:user)}
+
+    it "as admin, returns status ok" do
+      patch ('/api/organization/public')
+      login_with_api(user)
       expect(response).to have_http_status(:success)
     end
   end
-  
+
+  describe "PATCH /api/organization/public" do
+    let!(:organization) {create(:organization)}
+
+    it "as public user, returns status unauthorized" do
+      patch ('/api/organization/public')
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+  end
+
+
 end
