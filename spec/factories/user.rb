97b9072832a41f641fc45796d10127fae4969f7d
Santiago Leon <santiagoleon747@gmail.com>
2021-06-23 09:12:26 -0300

    client user on factory users, test with invalid parameters and invalid permissions

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,7 +6,16 @@ FactoryBot.define do
     last_name {Faker::Name.last_name}
     password {"12345678"}
     password_confirmation {"12345678"}
-    role_id { 1 }
+    role_id {Role.where(name: 'admin').first&.id}
+  end
+
+  factory :client_user, class: 'User' do
+    email {Faker::Internet.email}
+    first_name {Faker::Name.first_name}
+    last_name {Faker::Name.last_name}
+    password {"12345678"}
+    password_confirmation {"12345678"}
+    role_id {Role.where(name: 'client').first&.id}
   end
 
 end
--- spec/requests/api/members_spec.rb
+++ spec/requests/api/members_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Members', type: :request do
   let (:valid_params) { attributes_for :member}
   let (:invalid_params) { attributes_for :param_missing_member}
   let (:user) { attributes_for :admin_user }
+  let (:common_user) { attributes_for :client_user }
 
   describe 'POST /api/members' do
 
@@ -17,6 +18,32 @@ RSpec.describe 'Members', type: :request do
       end
       it 'succesfully created a new member' do
         expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with invalid parameters' do
+      before do
+        FactoryBot.create(:admin)
+        login_with_api(user)
+        post '/api/members', headers:{
+          'Authorization': json_response[:user][:token]},
+          :params => { member: invalid_params }
+      end
+      it 'request with invalid params' do
+        expect(response).to have_http_status(:bad_request)
+      end
+    end
+
+    context 'with common user' do
+      before do
+        FactoryBot.create(:client)
+        login_with_api(common_user)
+        post '/api/members', headers:{
+          'Authorization': json_response[:user][:token]},
+          :params => { member: invalid_params }
+      end
+      it 'request with unauthorize permissions' do
+        expect(response).to have_http_status(:unauthorized)
       end
     end
 
