8554ea2011981529ffb57766d8a76cdf11a68347
Alkemy <alkemydevonrails@gmail.com>
2021-06-23 15:19:06 -0300

    user and organization updated

--- spec/factories/role.rb
+++ spec/factories/role.rb
@@ -1,6 +1,14 @@
-FactoryBot.define do 
-  factory :role do
-    name {"admin"}
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :admin_role, class: 'Role' do
+    name { 'admin' }
+    description { 'administrator' }
+  end
+
+  factory :client_role, class: 'Role' do
+    name { 'client' }
+    description { 'client' }
   end
   
 end
