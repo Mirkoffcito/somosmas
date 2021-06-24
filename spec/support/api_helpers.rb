f3ca0fda52a68bbb04c6867371d3fd7b172d19e3
Alkemy <alkemydevonrails@gmail.com>
2021-06-24 18:25:29 -0300

    api helpers

--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -3,23 +3,10 @@ module Request
     def login_with_api(user)
       post '/api/auth/login', params: {
         user: {
-          email: user[:email],
-          password: user[:password]
-          }
+          email: user.email,
+          password: user.password
         }
-    end
-
-    def register_with_api(user)
-      post '/api/auth/register', params: {
-        user:{ 
-          first_name: user[:first_name],
-          last_name: user[:last_name],
-          email: user[:email],
-          password: user[:password],
-          password_confirmation: user[:password_confirmation],
-          role_id: user[:role_id]
-         }
       }
-    end
+    end 
   end
 end
