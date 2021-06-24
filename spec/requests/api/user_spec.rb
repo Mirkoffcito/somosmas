781444cc0dbbe734960f880f1aae33198cdfe411
Jose Figueroa <joselfigueroag@gmail.com>
2021-06-23 23:51:41 -0400

    improved testing

--- spec/requests/api/user_spec.rb
+++ spec/requests/api/user_spec.rb
@@ -66,20 +66,92 @@ RSpec.describe "Users", type: :request do
         @headers = { 'Authorization' => json_response[:user][:token] }
       end
       
-      it 'update without token, unauthorized' do
-        json_response[:user][:first_name] = Faker::Name.first_name
-        @params = { "user":{ "first_name": json_response[:user][:first_name] }} 
-        patch "/api/users/#{@id}", :params => @params
-
-        expect(response).to have_http_status(:unauthorized)
-      end
-      
       it 'update first name correctly' do
         json_response[:user][:first_name] = 'Jose'
         @params = { "user":{ "first_name": json_response[:user][:first_name] }}
         patch "/api/users/#{@id}", :params => @params, :headers => @headers
 
         expect(json_response[:user][:first_name]).to eq('Jose')
+      end
+
+      it 'update first name other id' do
+        @client_user.first_name = 'Pedro'
+        @params = { "user":{ "first_name": @client_user.first_name }}
+        patch "/api/users/#{@client_user.id}", :params => @params, :headers => @headers
+
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'as client' do
+      before(:each) do
+        login_with_api(@client_user)
+        @id = json_response[:user][:id]
+        @headers = { 'Authorization' => json_response[:user][:token] }
+      end
+      
+      it 'update first name correctly' do
+        json_response[:user][:first_name] = 'Jose'
+        @params = { "user":{ "first_name": json_response[:user][:first_name] }}
+        patch "/api/users/#{@id}", :params => @params, :headers => @headers
+        
+        expect(json_response[:user][:first_name]).to eq('Jose')
+      end
+      
+      it 'update without token, unauthorized' do
+        json_response[:user][:first_name] = Faker::Name.first_name
+        @params = { "user":{ "first_name": json_response[:user][:first_name] }} 
+        patch "/api/users/#{@id}", :params => @params
+  
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe 'Delete /destroy' do
+    context 'as admin' do
+      before(:each) do
+        login_with_api(@admin_user)
+        @id = json_response[:user][:id]
+        @headers = { 'Authorization' => json_response[:user][:token] }
+      end
+
+      it 'with token message: Succesfully deleted' do
+        delete "/api/users/#{@id}", :headers => @headers
+        expect(response.body).to include("Succesfully deleted")  
+      end
+
+      it 'without token' do
+        delete "/api/users/#{@id}"
+        expect(response.status).to eq(401)  
+      end
+
+      it 'with token for other id' do
+        delete "/api/users/#{@client_user.id}", :headers => @headers
+        expect(response.status).to eq(401)
+      end
+    end
+
+    context 'as client' do
+      before(:each) do
+        login_with_api(@client_user)
+        @id = json_response[:user][:id]
+        @headers = { 'Authorization' => json_response[:user][:token] }
+      end
+
+      it 'with token' do
+        delete "/api/users/#{@id}", :headers => @headers
+        expect(response.status).to eq(200)  
+      end
+
+      it 'without token message: Unauthorized access.' do
+        delete "/api/users/#{@id}"
+        expect(response.body).to include("Unauthorized access.")  
+      end
+
+      it 'with token for other id' do
+        delete "/api/users/#{@admin_user.id}", :headers => @headers
+        expect(response.status).to eq(401)
       end
     end
   end
