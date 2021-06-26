88abd3bdbd1fb8532268073d83eecb2b5dda954e
Alkemy <alkemydevonrails@gmail.com>
2021-06-26 12:00:10 -0300

    updates

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -2,12 +2,12 @@
 
 module Api
   class OrganizationsController < ApplicationController
-    skip_before_action :authenticate_admin, only: [:index]
-    skip_before_action :authorize_request, only: [:index]
+    skip_before_action :authenticate_admin, only: [:get_organization]
+    skip_before_action :authorize_request, only: [:get_organization]
 
-    def index
+    def get_organization
       @organization = Organization.first
-
+      return render json: {}, status: :ok if @organization.blank?
       render json: @organization, serializer: OrganizationSerializer
     end
 
--- spec/requests/api/organization_spec.rb
+++ spec/requests/api/organization_spec.rb
@@ -6,26 +6,26 @@ RSpec.describe 'Organizations', type: :request do
   let (:attributes) { attributes_for :organization }
 
   describe 'GET /api/organization/public' do
-    # context "when organization's table is empty" do
-    #   before { get '/api/organization/public' }
-    #   it 'does return a HTTP STATUS 200' do
-    #     expect(response).to have_http_status(:ok)
-    #   end
-    #   it 'does return an empty array' do
-    #     expect(json_response[:organization]).to eq([])
-    #   end
-    # end
+    context "when organization's table is empty" do
+      before { get '/api/organization/public' }
+      it 'does return a HTTP STATUS 200' do
+        expect(response).to have_http_status(:ok)
+      end
+      it 'returns an empty array' do
+        expect(json_response[:organization]).to eq(nil)
+      end
+    end
 
     context "when organization's table is not empty" do
       before do
         create(:organization)
         get '/api/organization/public'
       end
-      it 'does return a HTTP STATUS 200' do
+      it 'returns a HTTP STATUS 200' do
         expect(response).to have_http_status(:ok)
       end
-      it 'does return an Organization' do
-        expect(json_response[:organization].length).to eq(10)
+      it 'returns an Organization' do
+        expect(json_response[:organization].length).to eq(11)
       end
       it 'Organization does have a name, email and welcome_text' do
         check_keys(json_response[:organization])
@@ -44,20 +44,20 @@ RSpec.describe 'Organizations', type: :request do
         @json_response = nil
         update_organization(attributes, @token)
       end
-      it 'does return a HTTP STATUS 401' do
+      it 'returns a HTTP STATUS 401' do
         expect(response).to have_http_status(:unauthorized)
       end
-      it 'does return a message error' do
+      it 'returns a message error' do
         expect(json_response[:error]).to eq('You are not an administrator')
       end
     end
 
     context "when user's not registered" do
       before { update_organization(attributes, '') }
-      it 'does return a HTTP STATUS 401' do
+      it 'returns a HTTP STATUS 401' do
         expect(response).to have_http_status(:unauthorized)
       end
-      it 'does return a message error' do
+      it 'returns a message error' do
         expect(json_response[:message]).to eq('Unauthorized access.')
       end
     end
@@ -72,10 +72,10 @@ RSpec.describe 'Organizations', type: :request do
     
       context 'when params are valid' do
         before { update_organization(attributes, @token) }
-        it 'does return a HTTP STATUS 200' do
+        it 'returns a HTTP STATUS 200' do
           expect(response).to have_http_status(:ok)
         end
-        it 'does return the updated organization' do
+        it 'returns the updated organization' do
           updated_organization = Organization.new(attributes)
           compare_organization(json_response, updated_organization)
         end
@@ -83,10 +83,10 @@ RSpec.describe 'Organizations', type: :request do
 
       context 'when params are empty' do
         before { update_organization('', @token) }
-        it 'does return a HTTP STATUS 400' do
+        it 'returns a HTTP STATUS 400' do
           expect(response).to have_http_status(:bad_request)
         end
-        it 'does return an error message' do
+        it 'returns an error message' do
           expect(json_response[:error]).to eq('Parameter is missing or its value is empty')
         end
       end
