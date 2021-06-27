64456d9f74e20e28963e1bb62631c0023058f999
Santiago Leon <santiagoleon747@gmail.com>
2021-06-27 00:17:02 -0300

    Removed member_helpers and added shared examples for code improvement

--- spec/requests/api/members_spec.rb
+++ spec/requests/api/members_spec.rb
@@ -3,140 +3,179 @@
 require 'rails_helper'
 
 RSpec.describe 'Members', type: :request do
+  shared_examples 'check key' do
+    it 'check that each member has keys name and description' do
+      json_response[:members].each do |member|
+        expect(member).to have_key(:name)
+        expect(member).to have_key(:description)
+      end
+    end
+  end
+
+  shared_examples 'compares members' do |subject|
+    let(:update_member) { Member.new(attributes) }
+    it "returns the #{ subject } name and description" do
+      expect(json_response[:member][:name]).to eq(update_member.name)
+      expect(json_response[:member][:description]).to eq(update_member.description)
+    end
+  end
+
   let (:attributes) { attributes_for :member }
 
   describe "GET /api/members" do
-    context "when member's table is empty" do
-      before { get '/api/members' }
+    subject(:get_members) { get '/api/members' }
 
-      it 'does return a HTTP STATUS 200' do
-        expect(response).to have_http_status(:ok)
+    context "when member's table is empty" do
+      before { get_members }
+      it 'returns an empty array' do
+        expect(json_response[:members]).to eq([])
       end
 
-      it 'does return an empty array' do
-        expect(json_response[:members]).to eq([])
+      it 'returns a HTTP STATUS 200' do
+        expect(response).to have_http_status(:ok)
       end
     end
 
     context "when member's table is not empty" do
       before do
         create_list(:member, 10, attributes)
-        get '/api/members'
+        get_members
       end
 
-      it 'does return a HTTP STATUS 200' do
+      it 'returns a HTTP STATUS 200' do
         expect(response).to have_http_status(:ok)
       end
 
-      it 'does return an array of members' do
+      it 'returns an array of members' do
         expect(json_response[:members].length).to eq(10)
       end
 
-      it 'each member does have a name and description' do
+      it 'returns correct name and description for each member' do
         check_keys(json_response[:members])
       end
     end
   end
 
   describe 'POST /api/members' do
+    subject(:create_member) {
+      post '/api/members',
+      headers:{ 'Authorization': token },
+      params:{ member: attributes }
+     }
+
     context "when user's admin'" do
+      let(:admin_user) { create(:user, :admin_user) }
+      let(:token) { json_response[:user][:token] }
       before do
-        admin_user = create(:user, :admin_user)
         login_with_api(admin_user)
-        @token = json_response[:user][:token]
+        token
         @json_response = nil
       end
 
       context 'when parameters are valid' do
-
         before do |example|
-          create_member(attributes, @token) unless example.metadata[:skip_before]
+          attributes[:name] = 'Random name'
+          attributes[:description] = 'Random description'
+          create_member unless example.metadata[:skip_before]
         end
 
-        it 'does add a member to database', :skip_before do
-          expect{ create_member(attributes, @token) }.to change(Member, :count).by(1)
+        it 'adds a member to database', :skip_before do
+          expect{ create_member }.to change(Member, :count).by(1)
         end
 
-        it 'does return a HTTP STATUS 200' do
+        it 'returns a HTTP STATUS 200' do
           expect(response).to have_http_status(:created)
         end
 
-        it 'does return the created member' do
-          member = Member.new(attributes)
-          compare_member(json_response, member)
-        end
+        include_examples 'compares members', 'created member'
       end
 
       context 'when parameters are invalid' do
         before do
           attributes[:name] = nil
-          create_member(attributes, @token)
+          create_member
         end
 
-        it 'does return a HTTP STATUS 400' do
+        it 'returns a HTTP STATUS 400' do
           expect(response).to have_http_status(:bad_request)
         end
 
+        it 'returns an error message' do
+          expect(json_response[:name]).to eq(["can't be blank"])
+        end
       end
     end
 
     context "when user's not admin" do
-      before { create_member(attributes, 'client_token') }
+      let(:token) { 'client_token' }
+      before { create_member }
 
-      it 'does return a HTTP STATUS 401' do
+      it 'returns a HTTP STATUS 401' do
         expect(response).to have_http_status(:unauthorized)
       end
 
-      it 'does return a message error' do
+      it 'returns a message error' do
         expect(json_response[:message]).to eq('Unauthorized access.')
       end
     end
   end
 
-  describe 'PUT /api/members' do
-    before { @member = create(:member, attributes) }
+  describe 'PUT /api/members/:id' do
+    let!(:member) { create(:member, attributes) }
 
+    subject(:updates_member) {
+      put "/api/members/#{id}",
+        headers: { 'Authorization': token },
+        params: { member: attributes }
+    }
     context "when user's not admin" do
-      before { update_member(@member.id, attributes, 'client_token') }
+      let(:token) { 'client_token' }
+      let(:id) { member.id }
+      before { updates_member }
 
-      it 'does return a HTTP STATUS 401' do
+      it 'returns a HTTP STATUS 401' do
         expect(response).to have_http_status(:unauthorized)
       end
 
-      it 'does return a message error' do
+      it 'returns a message error' do
         expect(json_response[:message]).to eq('Unauthorized access.')
       end
     end
 
     context "when user's admin" do
+      let(:admin_user) { create(:user, :admin_user) }
+      let(:token) { json_response[:user][:token] }
       before do
-        admin_user = create(:user, :admin_user)
         login_with_api(admin_user)
-        @token = json_response[:user][:token]
+        token
         @json_response = nil
       end
 
       context 'when params are valid' do
-        before { update_member(@member.id, attributes, @token) }
+        let(:id) { member.id }
+        before do
+          attributes[:name] = 'Random name'
+          attributes[:description] = 'Random description'
+          updates_member
+        end
 
-        it 'does return a HTTP STATUS 200' do
+        it 'returns a HTTP STATUS 200' do
           expect(response).to have_http_status(:ok)
         end
 
-        it 'does return the updated member' do
-          updated_member = Member.new(attributes)
-          compare_member(json_response, updated_member)
-        end
+        include_examples "compares members", 'updated member'
       end
 
       context 'when params are empty' do
-        before { update_member(@member.id, '', @token) }
+        let(:id) { member.id }
+        let(:attributes) { }
+        before{ updates_member }
 
-        it 'does return a HTTP STATUS 400' do
+        it 'returns a HTTP STATUS 400' do
           expect(response).to have_http_status(:bad_request)
         end
 
-        it 'does return an error message' do
+        it 'returns an error message' do
           expect(json_response[:error]).to eq('Parameter is missing or its value is empty')
         end
       end
@@ -144,54 +183,62 @@ RSpec.describe 'Members', type: :request do
   end
 
   describe 'DELETE /api/members' do
-    before { @member = create(:member, attributes) }
-
+    let (:member) { create(:member, attributes) }
+    let (:id) { member.id }
+    subject(:delete_member) {
+      delete "/api/members/#{id}",
+        headers: { 'Authorization': token }
+    }
     context "when user's not admin" do
-      before { delete_member(@member.id, 'client_token') }
-
-      it 'does return a HTTP STATUS 401' do
+      let(:token) { 'client_token' }
+      before { delete_member }
+      
+      it 'returns a HTTP STATUS 401' do
         expect(response).to have_http_status(:unauthorized)
       end
 
-      it 'does return an error message' do
+      it 'returns an error message' do
         expect(json_response[:message]).to eq('Unauthorized access.')
       end
     end
 
     context 'when user is admin' do
+      let(:token) { json_response[:user][:token] }
+      let(:admin_user) { create(:user, :admin_user) }
       before do
-        admin_user = create(:user, :admin_user)
+        member
         login_with_api(admin_user)
-        @token = json_response[:user][:token]
+        token
         @json_response = nil
       end
 
       context 'when member is deleted' do
         before do |example|
-          delete_member(@member.id, @token) unless example.metadata[:skip_before]
+          delete_member unless example.metadata[:skip_before]
         end
 
-        it 'does delete member from database', :skip_before do
-          expect{ delete_member(@member.id, @token) }.to change(Member, :count).by(-1)
+        it 'deletes member from database', :skip_before do
+          expect{ delete_member }.to change(Member, :count).by(-1)
         end
 
-        it 'does return HTTP STATUS 200' do
+        it 'returns HTTP STATUS 200' do
           expect(response).to have_http_status(:ok)
         end
 
-        it 'does return a success message' do
+        it 'returns a success message' do
           expect(json_response[:message]).to eq('Succesfully deleted')
         end
       end
 
       context "when member's not found" do
-        before { delete_member(99, @token) }
+        let(:id) { 99 }
+        before { delete_member }
 
-        it 'does return a HTTP STATUS 404' do
+        it 'returns a HTTP STATUS 404' do
           expect(response).to have_http_status(:not_found)
         end
 
-        it 'does return a member not found message' do
+        it 'returns a member not found message' do
           expect(json_response[:error]).to eq('member not found')
         end
       end
--- spec/support/member_helpers.rb
+++ spec/support/member_helpers.rb
@@ -1,22 +1,5 @@
 module Request
   module MemberHelpers
-    
-    def compare_member(response, member)
-      expect(response[:member][:name]).to eq(member.name)
-      expect(response[:member][:description]).to eq(member.description)
-    end
-
-    def create_member(attributes, token)
-      post '/api/members',
-        headers: { 'Authorization': token},
-        params: { member: attributes }
-    end
-    
-    def update_member(id, attributes, token)
-      put "/api/members/#{id}",
-        headers: { 'Authorization': token},
-        params: { member: attributes }
-    end
 
     def delete_member(id, token)
       delete "/api/members/#{id}",
