25c1beda9d8e41781efc66e93e99b189115a3e18
Jose Figueroa <joselfigueroag@gmail.com>
2021-06-28 15:19:15 -0400

    latest improvements in user testing

--- spec/requests/api/user_spec.rb
+++ spec/requests/api/user_spec.rb
@@ -66,62 +66,115 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'PATCH api/users/:id' do
-    subject { patch "/api/users/#{@id}", params: @params, headers: @headers }
-
     context 'when users is not logged' do
-      before(:each) do
-        login_with_api(@admin_user)
-        @id = json_response[:user][:id]
-        @headers = { 'Authorization' => json_response[:user][:token] }
-      end
-
       it 'returns a status unauthorized' do
-        json_response[:user][:first_name] = 'Otro nombre'
-        @params = { user: { first_name: json_response[:user][:first_name] } }
+        @id = 1
+        @params = { user: { first_name: 'Otro nombre' } }
         patch "/api/users/#{@id}", params: @params
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
     context 'when user is logged' do
-      before(:each) do
-        login_with_api(@admin_user)
-        @id = json_response[:user][:id]
-        @headers = { 'Authorization' => json_response[:user][:token] }
-      end
+      context 'when user is client' do
+        subject { patch "/api/users/#{@id}", params: @params, headers: @headers }
 
-      context 'when correct params' do
-        it 'changes the name correctly' do
-          json_response[:user][:first_name] = 'Jose'
-          @params = { user: { first_name: json_response[:user][:first_name] } }
-          subject
-          expect(json_response[:user][:first_name]).to eq('Jose')
-          expect(response).to have_http_status(:ok)
+        before(:each) do
+          login_with_api(@client_user)
+          @id = json_response[:user][:id]
+          @headers = { 'Authorization' => json_response[:user][:token] }
+        end
+
+        context 'when correct params' do
+          it 'changes the name correctly' do
+            json_response[:user][:first_name] = 'Luis'
+            @params = { user: { first_name: json_response[:user][:first_name] } }
+            subject
+            expect(json_response[:user][:first_name]).to eq('Luis')
+          end
+
+          it 'returns a status ok' do
+            expect(response).to have_http_status(:ok)
+          end
+        end
+
+        context 'when invalid or missing params' do
+          it 'returns error with last name blank' do
+            json_response[:user][:last_name] = ' '
+            @params = { user: { last_name: json_response[:user][:last_name] } }
+            subject
+            expect(response.body).to include("can't be blank")
+          end
+
+          it 'returns error with invalid email' do
+            json_response[:user][:email] = 'correo-no-valido'
+            @params = { user: { email: json_response[:user][:email] } }
+            subject
+            expect(response.body).to include('is invalid')
+          end
+
+          it 'returns a status bad request without params' do
+            patch "/api/users/#{@id}", headers: @headers
+            expect(response).to have_http_status(:bad_request)
+          end
+
+          it 'returns a status forbidden' do
+            @client_user.first_name = 'Pedro'
+            @params = { user: { first_name: @client_user.first_name } }
+            patch "/api/users/#{@admin_user.id}", params: @params, headers: @headers
+            expect(response).to have_http_status(:forbidden)
+          end
         end
       end
 
-      context 'when invalid or missing params' do
-        it 'returns error and a status bad request with last name blank' do
-          json_response[:user][:last_name] = ' '
-          @params = { user: { last_name: json_response[:user][:last_name] } }
-          subject
-          expect(response.body).to include("can't be blank")
-          expect(response).to have_http_status(:bad_request)
+      context 'when user is admin' do
+        subject { patch "/api/users/#{@id}", params: @params, headers: @headers }
+
+        before(:each) do
+          login_with_api(@admin_user)
+          @id = json_response[:user][:id]
+          @headers = { 'Authorization' => json_response[:user][:token] }
         end
 
-        it 'returns error and a status bad request with invalid email' do
-          json_response[:user][:email] = 'correo-no-valido'
-          @params = { user: { email: json_response[:user][:email] } }
-          subject
-          expect(response.body).to include('is invalid')
-          expect(response).to have_http_status(:bad_request)
+        context 'when correct params' do
+          it 'changes the name correctly' do
+            json_response[:user][:first_name] = 'Jose'
+            @params = { user: { first_name: json_response[:user][:first_name] } }
+            subject
+            expect(json_response[:user][:first_name]).to eq('Jose')
+          end
+
+          it 'returns a status ok' do
+            expect(response).to have_http_status(:ok)
+          end
         end
 
-        it 'returns a status forbidden' do
-          @client_user.first_name = 'Pedro'
-          @params = { user: { first_name: @client_user.first_name } }
-          patch "/api/users/#{@client_user.id}", params: @params, headers: @headers
-          expect(response).to have_http_status(:forbidden)
+        context 'when invalid or missing params' do
+          it 'returns error with last name blank' do
+            json_response[:user][:last_name] = ' '
+            @params = { user: { last_name: json_response[:user][:last_name] } }
+            subject
+            expect(response.body).to include("can't be blank")
+          end
+
+          it 'returns error with invalid email' do
+            json_response[:user][:email] = 'correo-no-valido'
+            @params = { user: { email: json_response[:user][:email] } }
+            subject
+            expect(response.body).to include('is invalid')
+          end
+
+          it 'returns a status bad request without params' do
+            patch "/api/users/#{@id}", headers: @headers
+            expect(response).to have_http_status(:bad_request)
+          end
+
+          it 'returns a status forbidden' do
+            @client_user.first_name = 'Pedro'
+            @params = { user: { first_name: @client_user.first_name } }
+            patch "/api/users/#{@client_user.id}", params: @params, headers: @headers
+            expect(response).to have_http_status(:forbidden)
+          end
         end
       end
     end
@@ -129,54 +182,83 @@ RSpec.describe 'Users', type: :request do
 
   describe 'DELETE api/users/:id' do
     context 'when users is not logged' do
-      before(:each) do
-        login_with_api(@client_user)
-        @id = json_response[:user][:id]
-        @headers = { 'Authorization' => json_response[:user][:token] }
-      end
-
       it 'returns a status unauthorized' do
-        json_response[:user][:first_name] = 'Otro nombre'
-        @params = { user: { first_name: json_response[:user][:first_name] } }
+        @id = 1
+        @params = { user: { first_name: 'Otro nombre' } }
         patch "/api/users/#{@id}", params: @params
         expect(response).to have_http_status(:unauthorized)
       end
     end
 
     context 'when user is logged' do
-      subject { delete "/api/users/#{@id}", headers: @headers }
+      context 'when user is client' do
+        subject { delete "/api/users/#{@id}", headers: @headers }
 
-      before(:each) do
-        login_with_api(@client_user)
-        @id = json_response[:user][:id]
-        @headers = { 'Authorization' => json_response[:user][:token] }
-      end
-
-      context 'when correct params' do
-        it 'returns a status ok and success message' do
-          subject
-          expect(response.body).to include('Succesfully deleted')
-          expect(response).to have_http_status(:ok)
+        before(:each) do
+          login_with_api(@client_user)
+          @id = json_response[:user][:id]
+          @headers = { 'Authorization' => json_response[:user][:token] }
         end
 
-        it 'deletes a user' do
-          expect do
+        context 'when correct params' do
+          it 'returns a status ok' do
             subject
-          end.to change { User.count }.by(-1)
+            expect(response).to have_http_status(:ok)
+          end
+
+          it 'deletes a user' do
+            expect do
+              subject
+            end.to change { User.count }.by(-1)
+          end
+        end
+
+        context 'when invalid or missing params' do
+          it 'returns a status forbidden for other :id' do
+            delete "/api/users/#{@admin_user.id}", headers: @headers
+            expect(response.body).to include('You do not have permission')
+            expect(response).to have_http_status(:forbidden)
+          end
+
+          it 'returns a status unauthorized' do
+            delete "/api/users/#{@id}"
+            expect(response).to have_http_status(:unauthorized)
+          end
         end
       end
 
-      context 'when invalid or missing params' do
-        it 'returns a status forbidden for other :id' do
-          delete "/api/users/#{@admin_user.id}", headers: @headers
-          expect(response.body).to include('You do not have permission')
-          expect(response).to have_http_status(:forbidden)
+      context 'when user is admin' do
+        subject { delete "/api/users/#{@id}", headers: @headers }
+
+        before(:each) do
+          login_with_api(@admin_user)
+          @id = json_response[:user][:id]
+          @headers = { 'Authorization' => json_response[:user][:token] }
         end
 
-        it 'returns a status unauthorized and error message' do
-          delete "/api/users/#{@id}"
-          expect(response.body).to include('Unauthorized access.')
-          expect(response).to have_http_status(:unauthorized)
+        context 'when correct params' do
+          it 'deletes a user' do
+            expect do
+              subject
+            end.to change { User.count }.by(-1)
+          end
+
+          it 'returns a status ok' do
+            expect(response).to have_http_status(:ok)
+          end
+        end
+
+        context 'when invalid or missing params' do
+          it 'returns a status forbidden for other :id' do
+            delete "/api/users/#{@client_user.id}", headers: @headers
+            expect(response.body).to include('You do not have permission')
+            expect(response).to have_http_status(:forbidden)
+          end
+
+          it 'returns a status unauthorized' do
+            delete "/api/users/#{@id}"
+            expect(response).to have_http_status(:unauthorized)
+          end
         end
       end
     end
