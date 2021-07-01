33ee38b69fad4eb2f5490495fa120328b56e8001
Santiago Leon <santiagoleon747@gmail.com>
2021-07-01 12:32:17 -0300

    Improvements on Member documentation

--- spec/integration/api/members_spec.rb
+++ spec/integration/api/members_spec.rb
@@ -17,29 +17,153 @@ RSpec.describe '../integration/api/members', type: :request do
         parameter name: :member, in: :body, schema: {
           type: :object,
           properties: {
-            name: { type: :string },
-            facebook_url: { type: :string },
-            linkedin_url: { type: :string },
-            instagram_url: { type: :string },
-            description: { type: :string }
+            member: {
+              type: :array,
+              items: {
+                properties: {
+                  id: { type: :integer },
+                  name: { type: :string },
+                  facebook_url: { type: :string, nullable: true },
+                  linkedin_url: { type: :string, nullable: true },
+                  instagram_url: { type: :string, nullable: true },
+                  description: { type: :string, nullable: true }
+
+                }
+              }
+            }
           },
           required: ['name']
         }
     
         response '201', 'member created' do
-          let(:member) { { name: 'foo' } }
+          schema type: :object,
+            properties: {
+              member: {
+                type: :array,
+                items:{
+                  properties: {
+                    id: { type: :integer },
+                    name: { type: :string },
+                    facebook_url: { type: :string, nullable: true },
+                    linkedin_url: { type: :string, nullable: true },
+                    instagram_url: { type: :string, nullable: true },
+                    description: { type: :string, nullable: true }
+                  }
+                }
+              }
+            }
           run_test!
         end
 
         response '401', 'unauthorized' do
-          let(:Authorization) {}
           run_test!
         end
     
         response '422', 'invalid request' do
-          let(:member) {}
           run_test!
         end
+      end
+
+      get 'Get all members' do
+        tags 'Members'
+        consumes 'application/json'
+        produces 'application/json'
+
+        response '200', 'list of members' do
+          schema type: :object,
+            properties: {
+                members: {
+                  type: :array,
+                  items:{
+                    properties: {
+                      id: { type: :integer },
+                      name: { type: :string },
+                      facebook_url: { type: :string, nullable: true },
+                      linkedin_url: { type: :string, nullable: true },
+                      instagram_url: { type: :string, nullable: true },
+                      description: { type: :string, nullable: true }
+                    }
+                  }
+                }
+            }
+          run_test!
+        end
+      end
+    end
+
+    path '/api/members/{id}' do
+
+      put 'Updates a member' do
+        tags 'Members'
+        security [Bearer: {}]
+        produces 'application/json'
+        consumes 'application/json'
+        parameter name: :Authorization, in: :header, type: :string
+        parameter name: :id, in: :path, type: :string
+        parameter name: :member, in: :body, schema: {
+          type: :object,
+          properties: {
+            name: { type: :string },
+            facebook_url: { type: :string, nullable: true },
+            linkedin_url: { type: :string, nullable: true },
+            instagram_url: { type: :string, nullable: true },
+            description: { type: :string, nullable: true }
+          }
+        }
+
+        response '200', 'updated successfully' do
+          schema type: :object,
+            properties: {
+              member: {
+                type: :array,
+                items:{
+                  properties: {
+                    id: { type: :integer },
+                    name: { type: :string },
+                    facebook_url: { type: :string, nullable: true },
+                    linkedin_url: { type: :string, nullable: true },
+                    instagram_url: { type: :string, nullable: true },
+                    description: { type: :string, nullable: true }
+                  }
+                }
+              }
+            }
+          run_test!
+        end
+
+        response '401', 'unauthorized' do
+          run_test!
+        end
+    
+        response '422', 'invalid request' do
+          run_test!
+        end
+
+        response '404', 'not found'do
+          run_test!
+        end
+      end
+
+      delete 'Delete a member' do
+        tags 'Members'
+        security [Bearer: {}]
+        produces 'application/json'
+        consumes 'application/json'
+        parameter name: :Authorization, in: :header, type: :string
+        parameter name: :id, in: :path, type: :string
+
+        response '200', 'updated successfully' do
+          run_test!
+        end
+
+        response '401', 'unauthorized' do
+          run_test!
+        end
+        
+        response '404', 'not found'do
+          run_test!
+        end
+
       end
     end
   end
