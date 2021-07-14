dadf091cc8fb73e87712f48fbcafdc1a5465d5af
Alkemy <alkemydevonrails@gmail.com>
2021-07-13 22:21:05 -0300

    Changes on chat and migration

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,8 +2,8 @@
 
 class Chat < ApplicationRecord
   acts_as_paranoid
-  has_many :messages
-  has_many :user
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :messages
 
   validates :user1, :user2, presence: true
 end
--- db/migrate/20210714001116_create_messages.rb
+++ db/migrate/20210714001116_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
     create_table :messages do |t|
       t.integer :user_id
       t.text :detail
-      t.boolean :modified
+      t.boolean :modified, default: false
 
       t.timestamps
     end
