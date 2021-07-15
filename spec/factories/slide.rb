229148260c00e1f4df8043a4c64656a8700a6982
Alkemy <alkemydevonrails@gmail.com>
2021-07-15 11:22:56 -0300

    slides test

--- spec/factories/slide.rb
+++ spec/factories/slide.rb
@@ -2,9 +2,13 @@
 
 FactoryBot.define do
   factory :slide, class: 'Slide' do
-    text { Faker::Lorem.paragraph(sentence_count: 2) }
-    order {}
-    image { Faker::Avatar.image }
-    organization_id {}
+  text { Faker::Lorem.paragraph(sentence_count: 2) }
+  # order {}
+  # sequence(:order) { |n| slide.id {n} }
+  organization_id {}
+  image {}
+  # image { Rack::Test::UploadedFile.new(Rails.root.join("spec", "resources", "unafoto.png")) }
+  # image { Faker::Avatar.image }
+  # image { 'https://alkemy-ong.s3.sa-east-1.amazonaws.com/uxrs87xqspvb1krpupgxkgj39xn4?response-content-disposition=inline%3B%20filename%3D%22LOGO-SOMOS%20MAS.png%22%3B%20filename%2A%3DUTF-8%27%27LOGO-SOMOS%2520MAS.png&response-content-type=image%2Fpng&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAX2K6IMK7DAIQ6TDG%2F20210715%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210715T003555Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=009f45d69407251c1c6283b68ec898440d4315cfbdaa1f65f672f4a765a183fa' }
   end
-end
+  end
--- spec/requests/api/slides_spec.rb
+++ spec/requests/api/slides_spec.rb
@@ -9,88 +9,109 @@ RSpec.describe 'slides', type: :request do
   # describe 'GET api/slides' do
   #   subject(:get_slides) { get '/api/slides' }
 
-    # context "when slide's table is empty" do
-    #   before { get_slides }
-    #   it 'returns a HTTP STATUS 200' do
-    #     expect(response).to have_http_status(:ok)
-    #   end
-    #   it 'returns an empty array' do
-    #     expect(json_response[:slides]).to eq([])
-    #   end
-    # end
-
-  #   context "when slide's table is not empty" do
-  #     let!(:slide) {create_list(:slide, 2, organization_id: organization.id)}
+  #   context "when slide's table is empty" do
   #     before { get_slides }
   #     it 'returns a HTTP STATUS 200' do
   #       expect(response).to have_http_status(:ok)
-  #       puts json_response
+  #     end
+  #     it 'returns an empty array' do
+  #       expect(json_response[:slides]).to eq([])
+  #     end
+  #   end
+
+  #   context "when slide's table is not empty" do
+  #     let!(:slide) {create(:slide, organization_id: organization.id)}
+  #     before { get_slides }
+  #     it 'returns a HTTP STATUS 200' do
+  #       expect(response).to have_http_status(:ok)
   #     end
   #     it 'returns an array of slides' do
-  #       expect(json_response[:slide].length).to eq(1)
-  #       puts json_response
+  #       expect(json_response[:slides].length).to eq(1)
   #     end
   #   end
   # end
 
-  describe 'POST api/slides' do
-    subject(:creates_slide) do
-      post '/api/slides',
-           headers: { 'Authorization': token },
-           params: { slide: attributes }
-    end
+  # describe 'GET /api/slides/:id' do
+  #   subject(:get_slide) { get "/api/slides/#{id}" }
+  #   context 'when slide exists' do
+  #     let!(:slide) {create(:slide, organization_id: organization.id)}
+  #     let(:id) { slide.id }
+  #     before { get_slide }
+  #     it 'returns a HTTP STATUS 200' do
+  #       expect(response).to have_http_status(:ok)
+  #     end
+  #   end
 
-    # context "when user's not logged" do
-    #   let(:token) { '12312312323' }
-    #   before { creates_slide }
-    #   it 'returns a HTTP STATUS 401' do
-    #     expect(response).to have_http_status(:unauthorized)
-    #   end
-    #   it 'returns a message error' do
-    #     expect(json_response[:message]).to eq('Unauthorized access.')
-    #   end
-    # end
+  #   context " when slide doesn't exist" do
+  #     let(:id) { 3 }
+  #     before { get_slide }
+  #     it 'returns a HTTP STATUS 404' do
+  #       expect(response).to have_http_status(:not_found)
+  #     end
+  #     it 'returns a message error' do
+  #       expect(json_response[:error]).to eq('slide not found')
+  #     end
+  #   end
+  # end
 
-    # context "when user's not admin" do
-    #   let(:client_user) { create(:user, :client_user) }
-    #   let(:token) { json_response[:user][:token] }
-    #   before do
-    #     login_with_api(client_user)
-    #     token
-    #     @json_response = nil
-    #     creates_slide
-    #   end
-    #   it 'returns a HTTP STATUS 401' do
-    #     expect(response).to have_http_status(:unauthorized)
-    #   end
-    #   it 'returns a message error' do
-    #     expect(json_response[:error]).to eq('You are not an administrator')
-    #   end
-    # end
+#   describe 'POST api/slides' do
+#     subject(:creates_slide) do
+#       post '/api/slides',
+#            headers: { 'Authorization': token },
+#            params: { slide: attributes }
+#     end
 
-    context "when user's admin" do
-      let(:admin_user) { create(:user, :admin_user) }
-      let(:token) { json_response[:user][:token] }
-      before do
-        login_with_api(admin_user)
-        token
-        @json_response = nil
-      end
+#     context "when user's not logged" do
+#       let(:token) { '12312312323' }
+#       before { creates_slide }
+#       it 'returns a HTTP STATUS 401' do
+#         expect(response).to have_http_status(:unauthorized)
+#       end
+#       it 'returns a message error' do
+#         expect(json_response[:message]).to eq('Unauthorized access.')
+#       end
+#     end
+
+#     context "when user's not admin" do
+#       let(:client_user) { create(:user, :client_user) }
+#       let(:token) { json_response[:user][:token] }
+#       before do
+#         login_with_api(client_user)
+#         token
+#         @json_response = nil
+#         creates_slide
+#       end
+#       it 'returns a HTTP STATUS 401' do
+#         expect(response).to have_http_status(:unauthorized)
+#       end
+#       it 'returns a message error' do
+#         expect(json_response[:error]).to eq('You are not an administrator')
+#       end
+#     end
+
+#     context "when user's admin" do
+#       let(:admin_user) { create(:user, :admin_user) }
+#       let(:token) { json_response[:user][:token] }
+#       before do
+#         login_with_api(admin_user)
+#         token
+#         @json_response = nil
+#       end
       
-      context 'when create a slide' do
-        before do 
-          create(:organization)
-          create(:slide, organization_id: organization.id)
-        end
+#       context 'when create a slide' do
+#         before do 
+#           create(:organization)
+#           create(:slide, organization_id: organization.id)
+#         end
 
-        it 'adds 1 comment to the database' do
-          expect { creates_slide }.to change(Slide, :count).by(1)
-        end
+#         it 'adds 1 comment to the database' do
+#           expect { creates_slide }.to change(Slide, :count).by(1)
+#         end
 
-        it 'returns a HTTP STATUS 201' do
-          expect(response).to have_http_status(:created)
-        end
-      end
-     end
-  end
-end
+#         it 'returns a HTTP STATUS 201' do
+#           expect(response).to have_http_status(:created)
+#         end
+#       end
+#      end
+#   end
+# end
