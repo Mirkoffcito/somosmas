82bc83555a4f221fff99a59e859f4e1d856009ef
Jose Figueroa <joselfigueroag@gmail.com>
2021-07-15 17:20:39 -0400

    class name in message model corrected, add line for nested attributes in chat model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -4,6 +4,7 @@ class Chat < ApplicationRecord
   acts_as_paranoid
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
+  accepts_nested_attributes_for :messages
 
   validates :user1, :user2, presence: true
 end
