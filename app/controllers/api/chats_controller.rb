fd9b5d2e976fc5c74d5aec4add22e2dd6283d455
lucia <lupe.diez@gmail.com>
2021-07-19 10:55:12 -0300

    fixing typos

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,9 +1,13 @@
-class ChatsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  class ChatsController < ApplicationController
 
 
     def index
         @chats = @current_user.chats
-        paginate @chats, per_page: 10, each_serializer: ChatsSerializer
-      end
+        paginate @chats, per_page: 10, each_serializer: ChatSerializer
+    end
 
+  end
 end
