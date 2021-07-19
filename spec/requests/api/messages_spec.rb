6c971c8bff6afd6bb8f0aca8c9f48e0ee59df240
Alkemy <alkemydevonrails@gmail.com>
2021-07-19 18:52:29 -0300

    fix

--- spec/requests/api/messages_spec.rb
+++ spec/requests/api/messages_spec.rb
@@ -163,92 +163,100 @@ RSpec.describe 'Messages', type: :request do
   end
   
   describe "PUT /api/chats/:id/messages" do
+    let(:chat) { create(:chat) }
+    let(:user) { create(:user, :client_user) }
+    let(:chat_user) { create(:chat_user, user_id: user.id) }
+    let(:message) { create(:message, chat_id: chat.id, user_id: user.id) }
+  
     subject(:update_message) do
-      patch "/api/chats/#{id}/messages",
+      patch "/api/chats/#{chat_id}/messages/#{id}",
         headers: { 'Authorization': token },
         params: { message: attributes }
     end
 
     context "when user's not logged in" do
       let(:token) { '' }
-      let(:id) { 1 }
-
+      let(:chat_id) { chat.id }
+      let(:id) { message.id }
       before { update_message }
 
       it 'returns a HTTP STATUS 401' do
         expect(response).to have_http_status(:unauthorized)
       end
-
       it 'returns an error message' do
         expect(json_response[:message]).to eq('Unauthorized access.')
       end
     end
     
     context 'when user is logged' do
-      let(:user) { create(:user, :client_user) }
       let(:token) { json_response[:user][:token] }
-
-      before do
-        login_with_api(user)
-        token
-        @json_response = nil
-      end
       
-      context 'when message id does not exist' do
+      context 'when message does not exist' do
+        let(:chat_id) { 1 }
         let(:id) { 99 }
-
-        before { update_message }
+        before do
+          login_with_api(user)
+          token
+          @json_response = nil
+          update_message
+        end
 
         it 'returns a HTTP STATUS 404' do
           expect(response).to have_http_status(:not_found)
         end
-
         it 'returns an error message' do
           expect(json_response[:error]).to eq('message not found')
         end
       end
 
-      context 'when message id exists' do
-        let(:chat) { create(:chat) }
-        let(:chat_user) { create(:chat_user, user_id: user.id) }
-        let(:message) { create(:message, chat_id: chat.id, user_id: user.id) }
+      context 'when message exists' do
+        let(:chat_id) { chat.id }
         let(:id) { message.id }
         
         context 'when trying to update a message from another person' do
           let(:token) { 'random_token' }
-
-          before { update_message }
+          before do
+            login_with_api(user)
+            token
+            @json_response = nil
+            update_message
+          end
 
           it 'returns a HTTP STATUS 401' do
             expect(response).to have_http_status(:unauthorized)
           end
-
           it 'returns an error message' do
             expect(json_response[:message]).to eq('Unauthorized access.')
           end
         end
 
         context 'when trying to update a message that is not the last one' do
+          let(:chat_id) { chat.id }
           let(:id) { 1 }
-          
-          before { update_message }
+          before do
+            login_with_api(user)
+            token
+            @json_response = nil
+            update_message
+          end
 
           it 'returns a HTTP STATUS 404' do
             expect(response).to have_http_status(:not_found)
           end
-
           it 'returns an error message' do
             expect(json_response[:error]).to eq('message not found')
           end
         end
 
         context 'when owner updates with params' do
-          before do |example|
+          let(:chat_id) { chat.id }
+          let(:id) { message.id }
+          before do
             attributes[:detail] = 'DETAIL TEST'
-            update_message unless example.metadata[:skip_before]
+            update_message 
           end
 
-          it 'returns a HTTP STATUS 200', :skip_before do
+          it 'returns a HTTP STATUS 200' do
             expect(response).to have_http_status(:ok)
           end
         end
