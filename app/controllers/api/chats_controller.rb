17e0aef6bffea6f2cede6cbcb8d4667531e87c44
Jose Figueroa <joselfigueroag@gmail.com>
2021-07-20 13:42:53 -0400

    refactor chats controller, included receiver and messages fields

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -13,22 +13,36 @@ module Api
       if chat_user.blank?
         chat = Chat.new
         chat.chat_users.build(user_id: @current_user.id)
-        chat.chat_users.build(user_id: params[:receiver])
+        chat.chat_users.build(user_id: receiver)
       else
         chat = chat_user.chat
       end
-
+      chat.messages.build(message_params)
+  
       if chat.save
         render json: chat, serializer: ChatSerializer, status: :created
       else
         render json: chat.errors, status: :bad_request
       end
     end
-
+    
     private
+    
+    def params_attr
+      params.require(:chat).permit(:receiver, message: [:detail])
+    end
+
+    def receiver
+      params_attr[:receiver]
+    end
+
+    def message_params
+      params_attr[:message].merge(user_id: @current_user.id)
+    end
 
     def chat_user
-      ChatUser.find_by(user_id: @current_user.id, chat_id: ChatUser.where(user_id: params[:receiver]).select(:chat_id))
+      @chat_user ||= ChatUser.find_by(user_id: @current_user.id, 
+                                      chat_id: ChatUser.where(user_id: receiver).select(:chat_id))
     end
   end
 end
