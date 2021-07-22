febd847127bf8c2d30f6c1fa1d0c0e7ca38987a7
Alkemy <alkemydevonrails@gmail.com>
2021-07-22 13:39:29 -0300

    Update

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 class MessageSerializer < ActiveModel::Serializer
-  
   attributes :id, :detail, :chat, :analysis
-  
+
   belongs_to :chat
 
   def analysis
-    url = 'https://sentim-api.herokuapp.com/api/v1/'.freeze
-    headers = { "Content-Type": "application/json" }.freeze
-    body = { "text": "#{self}" }
-    response = RestClient::Request.execute(method: :post, :url => url, :headers => headers, :payload => body.to_json)
+    url = 'https://sentim-api.herokuapp.com/api/v1/'
+    headers = { "Content-Type": 'application/json' }.freeze
+    body = { "text": to_s }
+    response = RestClient::Request.execute(method: :post, url: url, headers: headers, payload: body.to_json)
     SentimentalAnalysis.analysis(response)
   end
-
 end
--- app/services/sentimental_analysis.rb
+++ app/services/sentimental_analysis.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 require 'json'
 
 class SentimentalAnalysis
-
   def self.analysis(response)
     if response.code == 200
-      response = JSON.parse(response) 
+      response = JSON.parse(response)
       response['result']
     else
-      response = { "error": "Message without sentimental analysis, caused by external API failure"}
+      response = { "error": 'Message without sentimental analysis, caused by external API failure' }
     end
   end
 end
