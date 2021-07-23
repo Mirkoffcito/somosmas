929b34d4b893e5e48304c60c151fcbd90226e4e5
Jose Figueroa <joselfigueroag@gmail.com>
2021-07-23 11:03:45 -0400

    refactor chats controller

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -14,23 +14,22 @@ module Api
         chat = Chat.new
         chat.chat_users.build(user_id: @current_user.id)
         chat.chat_users.build(user_id: receiver)
+        chat.messages.build(message_params) if message_params
+
+        if chat.save
+          render json: chat, serializer: ChatSerializer, status: :created
+        elsif chat_user.nil?
+          render json: { error: 'User not found' }, status: :not_found
+        else
+          render json: chat.errors, status: :bad_request
+        end
       else
-        chat = chat_user.chat
-      end
-      
-      if message_params
-        chat.messages.build(message_params)
-      end
-  
-      if chat.save
-        render json: chat, serializer: ChatSerializer, status: :created
-      else
-        render json: chat.errors, status: :bad_request
+        render json: { message: "Chat already exists, chat N° #{chat_user.chat.id}" }
       end
     end
-    
+
     private
-    
+
     def params_attr
       params.require(:chat).permit(:receiver, message: [:detail])
     end
@@ -40,13 +39,11 @@ module Api
     end
 
     def message_params
-      if params_attr[:message]
-        params_attr[:message].merge(user_id: @current_user.id)
-      end
+      params_attr[:message]&.merge(user_id: @current_user.id) 
     end
 
     def chat_user
-      @chat_user ||= ChatUser.find_by(user_id: @current_user.id, 
+      @chat_user ||= ChatUser.find_by(user_id: @current_user.id,
                                       chat_id: ChatUser.where(user_id: receiver).select(:chat_id))
     end
   end
